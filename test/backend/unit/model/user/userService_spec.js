5c5e447b06ad9c50fb08911e68ec3b59a824f7b3
David Rey <david.rey.acosta@gmail.com>
2015-08-17 00:16:20 +0200

    Moved calculateMyCommunity unit tests to userService unit tests

--- test/backend/unit/model/user/userService_spec.js
+++ test/backend/unit/model/user/userService_spec.js
@@ -1,20 +1,20 @@
 'use strict';
 
 var assert = require('assert');
-var calculateMyCommunity = require('../../../../../src/core/infrastructure/services/calculateMyCommunity');
+var userService = require('../../../../../src/core/model/user/userService');
 
 describe('Calculate my Twitter community', function() {
   it('should calcula 1500 members (1250 followers + 250 followings', function() {
     var followers = 1250;
     var followings = 250;
-    var myCommunity = calculateMyCommunity(followers, followings);
+    var myCommunity = userService.calculateMyCommunity(followers, followings);
     assert.equal(myCommunity, 1500);
   });
 
   it('should calcula 1500 members (1723 followers + 2039 followings', function() {
     var followers = 1723;
     var followings = 2039;
-    var myCommunity = calculateMyCommunity(followers, followings);
+    var myCommunity = userService.calculateMyCommunity(followers, followings);
     assert.equal(myCommunity, 3762);
   });
 });
